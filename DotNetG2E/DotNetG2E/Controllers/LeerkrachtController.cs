32a0903396ebd238168f3fd68add26ca827d77d1
Lorenz <lorenz.baele.v6128@student.hogent.be>
2018-05-15 23:12:22 +0200

    User can now active/deactivate a session according to DR rules

--- DotNetG2E/DotNetG2E/Controllers/LeerkrachtController.cs
+++ DotNetG2E/DotNetG2E/Controllers/LeerkrachtController.cs
@@ -44,12 +44,12 @@ namespace DotNetG2E.Controllers
         {
 
             //untill db error is fixed
-            //Pupil p1 = new Pupil() { Name = "Wannes" };
-            //ICollection<Pupil> pList1 = new List<Pupil>();
-            //pList1.Add(p1);
-            //Sessie sessie = _sessieRepository.GetBy(id);
-            //sessie.Groups.Add(new Group() { Name = "2B", Pupils = pList1 });
-            //ViewBag.sessie = sessie;
+            Pupil p1 = new Pupil() { Name = "Wannes" };
+            ICollection<Pupil> pList1 = new List<Pupil>();
+            pList1.Add(p1);
+            Sessie sessie = _sessieRepository.GetBy(id);
+            sessie.Groups.Add(new Group() { Name = "2B", Pupils = pList1 , Selected = false});
+            ViewBag.sessie = sessie;
             //-----
 
             ViewBag.sessie = _sessieRepository.GetBy(id);
@@ -57,14 +57,35 @@ namespace DotNetG2E.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult ActiveerSessie(int id)
-        //{
-        //    Sessie sessie = _sessieRepository.GetBy(id);
-        //    sessie.IsActive = true;
-        //    _sessieRepository.SaveChanges();
+        
+        public IActionResult ActiveerSessie(int id)
+        {
+            Sessie sessie = _sessieRepository.GetBy(id);
+            sessie.IsActive = true;
+            Pupil p1 = new Pupil() { Name = "Wannes" };
+            ICollection<Pupil> pList1 = new List<Pupil>();
+            pList1.Add(p1);
+            sessie.Groups.Add(new Group() { Name = "2B", Pupils = pList1, Selected = false });
+            ViewBag.sessie = sessie;
+            //-----
+            _sessieRepository.SaveChanges();
+            ViewBag.sessie = _sessieRepository.GetBy(id);
+            return View("~/Views/Leerkracht/Sessie.cshtml");
+        }
 
-        //    return RedirectToAction(nameof(Index));
-        //}
+        public IActionResult DeactiveerSessie(int id)
+        {
+            Sessie sessie = _sessieRepository.GetBy(id);
+            sessie.IsActive = false;
+            Pupil p1 = new Pupil() { Name = "Wannes" };
+            ICollection<Pupil> pList1 = new List<Pupil>();
+            pList1.Add(p1);
+            sessie.Groups.Add(new Group() { Name = "2B", Pupils = pList1, Selected = false });
+            ViewBag.sessie = sessie;
+            //-----
+            _sessieRepository.SaveChanges();
+            ViewBag.sessie = _sessieRepository.GetBy(id);
+            return View("~/Views/Leerkracht/Sessie.cshtml");
+        }
     }
 }
